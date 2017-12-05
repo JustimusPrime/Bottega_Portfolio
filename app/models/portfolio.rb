437f6377cb4321bf8cb6268d8de5c8814f040348
Justimus Prime <jmurray@Justimuss-MBP.ad.uvu.edu>
2017-12-05 13:58:46 -0700

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,15 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+#above are two different ways to create custom scope
+
+	after_initialize :set_defaults
+
+	def set_defaults 
+		self.main_image ||= "http://via.placeholder.com/600x400"
+		self.thumb_image ||= "http://via.placeholder.com/350x200"
+	end
+# above is another way to set defaults
 end
 
-#above are two different ways to create custom scope
+# ||= means if the item is nil, then set it to this
