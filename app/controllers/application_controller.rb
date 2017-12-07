e4cb10422555239636c0e8d4a7ae24584f596887
Justimus Prime <jmurray@Justimuss-MBP.ad.uvu.edu>
2017-12-07 13:58:14 -0700

    refactored app controller and added query, Added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  include SetSource
 end
+
+# everything done here can be accessed by child controllers, that said you want to keep it clean
