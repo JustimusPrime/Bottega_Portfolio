ff912497f9903f2cfaafa56dba72cad6c3c53848
JustimusPrime <hirejustin@yahoo.com>
2017-12-08 11:17:23 -0700

    added nav bar and default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContent
+
 end
 # everything done here can be accessed by child controllers, that said you want to keep it clean. eventually you want to move things out. good for testing becuse code placement cant be the issue
