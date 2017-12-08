950c4e2d48da97090dc394b943560fc0b6a7a76d
JustimusPrime <hirejustin@yahoo.com>
2017-12-07 18:15:07 -0700

    Added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+  include CurrentUserConcern
 end
-
-# everything done here can be accessed by child controllers, that said you want to keep it clean
+# everything done here can be accessed by child controllers, that said you want to keep it clean. eventually you want to move things out. good for testing becuse code placement cant be the issue
